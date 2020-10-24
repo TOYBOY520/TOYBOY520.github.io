a7d1bc32fa2b23c1ee46d8edc22abfdbdbb5e3eb
B18031532黎帅帅 <3281407022@qq.com>
2020-10-24 12:58:31 +0800

    Update landing.js

--- landing.js
+++ landing.js
@@ -4,7 +4,7 @@ var box1=document.getElementById("in1").value;
 var box2=document.getElementById("in2").value;
 if(box1=="lss" && box2=="123456")
    {
-     window.location.href="index.html";
+     window.location.href="theme.html";
    }    
 else alert("用户名或密码有误!");
 }
